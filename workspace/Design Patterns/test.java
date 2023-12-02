791ead6e4539dbe2c429012993a8a0b352bb08c8
KosKati <KosKati81@gmail.com>
2023-12-02 16:05:35 +0100

    new version

--- workspace/Design Patterns/test.java
+++ workspace/Design Patterns/test.java
@@ -4,6 +4,6 @@ public class test
        public static void main (String[] args)
        {
              // Ausgabe Hello World!
-             System.out.println("Hello World!");
+             System.out.println("Hello World! now");
        }
 }
